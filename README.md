e2ba994434478bc17f899221d5871a7e4f158fc4
arielmoguillansky <arielmogui92@gmail.com>
2023-11-05 21:48:58 -0300

    env setup

---
+++ README.md
@@ -0,0 +1,19 @@
+# Agenda app
+
+### Stack
+
+- Laravel
+- Nuxt JS
+- Mysql
+- Phpmyadmin
+
+### Architecture
+
+- Backend: agenda-backend-app
+- Frontend: agenda-frontend-app
+
+### Setup
+
+- Download both agenda-app folders and docker-compose file
+- Make sure no containers are running on ports used by this app
+- Execute docker compose up
---
+++ agenda-backend-app/app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        try {
+            DB::connection()->getPdo();
+        } catch (\Exception $e) {
+            die("Could not connect to the database.  Please check your configuration. error:" . $e );
+        }
+    }
+}
